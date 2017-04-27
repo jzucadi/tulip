fbda6f015305273bd2d508ac51c767399d368259
James Zaccardo <jameszaccardo@gmail.com>
2017-04-27 13:50:20 -0700

    added extra component

---
+++ resources/pages/me.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    
+    <h1 class="title">
+      Me section
+    </h1>
+    <nuxt-link class="button" to="/">
+      Home
+    </nuxt-link>
+  </section>
+</template>
+
+<template>
+<section class="container">
+
+<section class="container"
+
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+
+
+</style>
