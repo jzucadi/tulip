311b5627b8a039b5c27b2cc5ba8073ab59a9180f
James Zaccardo <jameszaccardo@gmail.com>
2017-04-26 18:00:50 -0700

    dot the Is

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -4,8 +4,11 @@
     <h1 class="title">
       Grapes Demo
     </h1>
-    <nuxt-link class="button" to="/bookmark">
+    <nuxt-link class="button" to="/about">
       About page
+    </nuxt-link>
+     <nuxt-link class="button" to="/bookmark">
+      Bookmark page
     </nuxt-link>
   </section>
 </template>
