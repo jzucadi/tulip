81fbdde7c2fd6f63bf37134274ca553637dfed65
James Zaccardo <jameszaccardo@gmail.com>
2017-06-06 22:27:30 -0700

    contact update

--- resources/pages/contact.vue
+++ resources/pages/contact.vue
@@ -4,21 +4,7 @@
     <h1 class="title">
       Contact
     </h1>
-    <nuxt-link class="button" to="/bookmark">
-      Bookmarks
-    </nuxt-link>
-    <nuxt-link class="button" to="/awards">
-      Awards
-    </nuxt-link>
-    <nuxt-link class="button" to="/about">
-      About
-    </nuxt-link>
-    <nuxt-link class="button" to="/about">
-      Funding
-    </nuxt-link>
-    <nuxt-link class="button" to="/">
-      Home
-    </nuxt-link>
+   
   </section>
 </template>
 
