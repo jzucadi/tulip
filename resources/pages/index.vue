e0c4c32033e9ef8d5f02461ee39db9a8c4335d66
James Zaccardo <jameszaccardo@gmail.com>
2017-06-09 18:00:51 -0700

    Fixed fonts

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -2,7 +2,7 @@
   <section class="container">
     
     <h1 class="title">
-     Hi, I'm James Zaccardo, a full stack webdeveloper based in Los Angeles.
+     Hi, I'm James Zaccardo, a full stack web developer with 6+ years experience based in Los Angeles.
     </h1>
  
   </section>
