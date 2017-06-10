b742d565ee813e2ef6f4d01a230347aff29d9687
James Zaccardo <jameszaccardo@gmail.com>
2017-06-09 20:23:23 -0700

    Fixed intro

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -2,7 +2,7 @@
   <section class="container">
     <div class="separator"></div>
     <h1 class="title">
-     Hi, I'm <span>James Zaccardo</span>, a full stack web developer with 6+ years experience based in Los Angeles.
+     Hello. I'm <span>James Zaccardo</span>, a full stack web developer with 6+ years experience based in Los Angeles.
     </h1>
  
   </section>
